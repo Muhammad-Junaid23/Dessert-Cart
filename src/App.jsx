a5351e103ca495d261151ccd870835516c08e548
Muhammad Junaid <lonelostforever@gmail.com>
2024-12-05 15:28:53 +0500

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
+import { Container } from '@mui/material';
+import Layout from './components/Layout';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'hsl(14, 86%, 42%)', // Red
+    },
+    secondary: {
+      main: 'hsl(159, 69%, 38%)', // Green
+    },
+    rose: {
+      50: 'hsl(20, 50%, 98%)',
+      100: 'hsl(13, 31%, 94%)',
+      300: 'hsl(14, 25%, 72%)',
+      400: 'hsl(7, 20%, 60%)',
+      500: 'hsl(12, 20%, 44%)',
+      900: 'hsl(14, 65%, 9%)',
+    },
+  },
+  typography: {
+    fontFamily: "'Red Hat Text', sans-serif",
+    body1: {
+      fontSize: '16px',
+      fontWeight: 400,
+    },
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 const App = () => {
   return (
-    <div>
-      <h1>welcome to Dessert card</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Layout />
+      </Container>
+    </ThemeProvider>
   );
 };
 

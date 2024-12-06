a5f2344ccea60e44de7b2aa1db902f3909cf97ba
Muhammad Junaid <lonelostforever@gmail.com>
2024-12-06 11:09:13 +0500

    cart added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,16 @@
 import { Box } from '@mui/material';
 import Grid2 from '@mui/material/Grid2';
+import Cart from './Cart/Cart';
 
 const Layout = () => {
   return (
     <Box sx={{ flexGrow: 1 }} mt={8}>
       <Grid2 container spacing={2}>
-        <Grid2 size={{ sm: 12, md: 9 }}>
+        <Grid2 size={{ sm: 12, md: 8 }}>
           <h1>Desserts</h1>
         </Grid2>
-        <Grid2 size={{ xs: 12, md: 3 }}>
-          <h1>item 1 in grid col</h1>
+        <Grid2 size={{ xs: 12, md: 4 }}>
+          <Cart />
         </Grid2>
       </Grid2>
     </Box>

458aff80f9459bf07f7f87c5beb0598c957247db
Muhammad Junaid <lonelostforever@gmail.com>
2024-12-06 14:19:20 +0500

    add dessert items

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,26 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import Grid2 from '@mui/material/Grid2';
 import Cart from './Cart/Cart';
+import Card from './Card';
 
 const Layout = () => {
   return (
     <Box sx={{ flexGrow: 1 }} mt={8}>
       <Grid2 container spacing={2}>
         <Grid2 size={{ sm: 12, md: 8 }}>
-          <h1>Desserts</h1>
+          <Typography variant='h4' fontWeight={700}>
+            Desserts
+          </Typography>
+          <Stack my={3} direction='row' gap={3} justifyContent='start' alignItems='start' flexWrap='wrap'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </Stack>
         </Grid2>
         <Grid2 size={{ xs: 12, md: 4 }}>
           <Cart />

4770b7baf8d49fcd194a65dfa75474341cd5cf0d
Muhammad Junaid <lonelostforever@gmail.com>
2024-12-16 13:01:25 +0500

    add navbar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Grid2, Box, Typography } from '@mui/material';
+import { Grid2, Box, Typography, Container } from '@mui/material';
 import Cart from '.././Cart';
 import Card from '.././Card';
 import inventoryData from '../../data.json';
@@ -23,25 +23,27 @@ const Layout = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }} mt={8}>
-      <Grid2 container spacing={2}>
-        <Grid2 size={{ sm: 12, md: 8 }}>
-          <Typography variant='h4' fontWeight={700} mb={'16px'}>
-            Desserts
-          </Typography>
-          <Grid2 container gap={2} spacing={2} direction={{ direction: { xs: 'column' } }} mb={5}>
-            {inventory.map((item, index) => (
-              <Grid2 key={index} size={{ xs: 12, sm: 6, lg: 4 }}>
-                <Card item={item} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
-              </Grid2>
-            ))}
+    <Container>
+      <Box sx={{ flexGrow: 1 }} mt={8}>
+        <Grid2 container spacing={2}>
+          <Grid2 size={{ sm: 12, md: 8 }}>
+            <Typography variant='h4' fontWeight={700} mb={'16px'}>
+              Desserts
+            </Typography>
+            <Grid2 container gap={2} spacing={2} direction={{ direction: { xs: 'column' } }} mb={5}>
+              {inventory.map((item, index) => (
+                <Grid2 key={index} size={{ xs: 12, sm: 6, lg: 4 }}>
+                  <Card item={item} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
+                </Grid2>
+              ))}
+            </Grid2>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4 }} position={{ md: 'fixed', xl: 'static' }} right={{ md: '1rem' }}>
+            <Cart inventory={inventory} handleRemove={handleRemove} clearCart={clearCart} />
           </Grid2>
         </Grid2>
-        <Grid2 size={{ xs: 12, md: 4 }} position={{ md: 'fixed', xl: 'static' }} right={{ md: '1rem' }}>
-          <Cart inventory={inventory} handleRemove={handleRemove} clearCart={clearCart} />
-        </Grid2>
-      </Grid2>
-    </Box>
+      </Box>
+    </Container>
   );
 };
 
